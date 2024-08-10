ac0c07220fa3d06bd065e72ec035b15ee68fb5a7
nweerasuriya <65176466+nweerasuriya@users.noreply.github.com>
2024-08-10 01:00:03 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,6 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../src'))
 
 # -- Project information -----------------------------------------------------
@@ -58,4 +57,3 @@ master_doc = 'index'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
