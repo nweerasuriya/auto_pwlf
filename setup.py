ff868a2dc261264a64189026f444cca71e3b0b47
Ned <nedeeshaw@virginmedia.com>
2024-08-10 15:27:15 +0100

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auto_pwlf",
-    version="0.1.0",
+    version="0.2.0",
     author="Nedeesha Weerasuriya",
     author_email="nedeeshawork@gmail.com",
     description="Automated piecewise linear fitting",
--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
 from .auto_pwlf import AutoPWLF
-__version__ = '0.1.0'
+__version__ = '0.2.0'
